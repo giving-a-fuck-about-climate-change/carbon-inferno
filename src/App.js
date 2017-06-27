b774e5f56169075a5be1ddc984d2b1bebda2f18a
grady-lad <ogradymartin7@gmail.com>
2017-06-27 22:55:17 +0200

    Adding react char graph and some info divs with test data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Line } from 'react-chartjs-2';
+import { dayData, weekData } from './data'
+import 'whatwg-fetch';
+
+const globalSubHeader = 'GLOBAL CO2 LEVEL';
+const diffPPMSubHeader = 'SINCE LAST WEEK';
+const diffPercentSubHeader = 'SINCE LAST WEEK (%)';
+
+ const InfoColDiv = ({ statInfo, subHeader }) => {
+  return (
+    <div className="info-col">
+      <div className="info-wrapper">
+        <div className="stat-info"> {statInfo} </div>
+        <div className="sub-header"> {subHeader} </div>
+      </div>
+    </div>
+  );
+ };
+
+const calculateAverage = (arr) => {
+  const totalAmount = arr.reduce((total, item) => {
+    return total + item.ppm;
+  },0);
+  return (totalAmount / arr.length);
+}
+
+const calculateDiff = (previous, current) => previous > current ? `- ${(previous - current).toFixed(2)}` : `+ ${(current - previous).toFixed(2)}`;
+
+const calculatePercentageDiff = (previous, current) => {
+  const diff = ((previous/current) / previous) * 100;
+  return diff.toFixed(2);
+}
+
+const getData = (label) => {
+  const { results } = weekData;
+  return results.map((data) => data[label]);
+}
+
+const data = {
+  labels: getData('date'),
+  datasets: [
+    {
+      label: 'PPM',
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(240, 153, 159, 0.81)',
+      borderColor: 'rgba(255, 107, 117, 0.81)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 8,
+      pointHoverRadius: 2,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: getData('ppm'),
+    }
+  ]
+}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    const currWeekStat = dayData.results[0].ppm;
+    const lastWeekAverage = calculateAverage(weekData.results);
+    this.state = {
+      todaysPpm: `${currWeekStat} PPM`,
+      ppmDiff: `${calculateDiff(lastWeekAverage, currWeekStat)} PPM`,
+      ppmPercentDiff: `${calculatePercentageDiff(lastWeekAverage, currWeekStat)} PPM`
+    };
+  }
+
   render() {
+    const { todaysPpm, ppmDiff, ppmPercentDiff } = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+       <div className="flex-grid">
+         <InfoColDiv statInfo={todaysPpm} subHeader={globalSubHeader}/>
+         <InfoColDiv statInfo={ppmDiff} subHeader={diffPPMSubHeader}/>
+         <InfoColDiv statInfo={ppmPercentDiff} subHeader={diffPercentSubHeader}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div style={{ width: '80%', margin: '0 auto'}}>
+         <Line data={data} width={600} height={250}/>
+      </div>
       </div>
     );
   }
---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+export const weekData = {
+  "count": 4,
+  "next": null,
+  "previous": null,
+  "results": [{
+    "ppm": 407.73,
+    "date": "2017-06-19"
+  }, {
+    "ppm": 408.31,
+    "date": "2017-06-20"
+  }, {
+    "ppm": 408.15,
+    "date": "2017-06-22"
+  }, {
+    "ppm": 408.35,
+    "date": "2017-06-23"
+  }]
+}
+
+export const dayData = {
+  "count": 14711,
+  "next": "http://carbondoomsday-test.herokuapp.com/api/measurements/co2/?limit=1&offset=1&ordering=-date",
+  "previous": null,
+  "results": [{
+    "ppm": "408.35",
+    "date": "2017-06-23"
+  }]
+}
