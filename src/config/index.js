29cca248bb624002983ecfebff6f143df75e0fe8
grady-lad <ogradymartin7@gmail.com>
2017-07-21 13:05:20 +0200

    Adding config file for urls

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+const environment = {
+  test: {
+    apiEndpoint: 'http://127.0.0.1:8000',
+  },
+  development: {
+    apiEndpoint: 'http://carbondoomsday-test.herokuapp.com',
+  },
+  staging: {
+    apiEndpoint: 'http://carbondoomsday-test.herokuapp.com',
+  },
+  production: {
+    apiEndpoint: 'http://carbondoomsday.herokuapp.com',
+  },
+}[process.env.NODE_ENV || 'development'];
+
+module.exports = Object.assign({
+  node_env: process.env.NODE_ENV || 'development',
+}, environment);
