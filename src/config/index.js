74266f9abf7a03596173c3003d5cb3c84471dcf7
grady-lad <ogradymartin7@gmail.com>
2017-07-22 15:35:49 +0200

    Updating api endpoints to use https

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ const environment = {
     apiEndpoint: 'http://127.0.0.1:8000',
   },
   development: {
-    apiEndpoint: 'http://carbondoomsday-test.herokuapp.com',
+    apiEndpoint: 'https://carbondoomsday-test.herokuapp.com',
   },
   staging: {
-    apiEndpoint: 'http://carbondoomsday-test.herokuapp.com',
+    apiEndpoint: 'https://carbondoomsday-test.herokuapp.com',
   },
   production: {
-    apiEndpoint: 'http://carbondoomsday-test.herokuapp.com',
+    apiEndpoint: 'https://carbondoomsday-test.herokuapp.com',
   },
 }[process.env.NODE_ENV || 'development'];
 
