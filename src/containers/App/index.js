77e1cc789e6c051fd6600974295af08377b7099a
grady-lad <ogradymartin7@gmail.com>
2017-09-01 18:23:40 +0200

    Removing service worker functionality

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -219,7 +219,6 @@ class App extends Component {
         <Header/>
         <div className="App">
           <div className="flex-grid-header">
-            <div> test div </div>
             <TimeChoiceHeader timeHeaderLinks={timeHeaderLinks.map(this.populateWithClickFuncs())}/>
           </div>
           <LoadingWrapper loading={loading}>
