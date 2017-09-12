9f84569f6cbbd7be19d1800f8a52bf59b79354a7
Stephanie Tassone <stephanie.tassone@gmail.com>
2017-09-12 15:34:52 +0200

    Add React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import loadScripts from './analytics';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {  BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ROOT, TEAM_PAGE } from './routes/index';
 import App from './containers/App';
+import Team from './containers/Team';
 import registerServiceWorker from './registerServiceWorker';
 
 loadScripts();
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route exact path={ROOT} component={App}/>
+            <Route path={TEAM_PAGE} component={Team}/>
+        </Switch>
+    </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
