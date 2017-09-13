42d0d2d38e6ef00768201d21128c998d6b358b68
Stephanie Tassone <stephanie.tassone@gmail.com>
2017-09-13 16:16:35 +0200

    Disable test

--- src/containers/App/tests/App.test.js
+++ src/containers/App/tests/App.test.js
@@ -4,5 +4,5 @@ import App from '..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App />, div); //eslint-disable-line
 });
