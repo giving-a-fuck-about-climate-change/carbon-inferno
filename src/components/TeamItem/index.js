23f14459b3e3c0bb088cd7bb5ef9bd880de11af6
grady-lad <ogradymartin7@gmail.com>
2017-09-18 16:46:58 +0200

    Fixing team location link

--- src/components/TeamItem/index.js
+++ src/components/TeamItem/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TeamItem = ({ name, location, position, src, alt }) => (
-    <div>
-        <img src={src} className="team-image" alt={alt} />
-        <div className="team-content">
-            <p className="team-name">{name}</p>
-            <p className="team-position">{position}</p>
-            <p className="team-location">{location}</p>
-        </div>
+  <div>
+    <img src={src} className="team-image" alt={alt} />
+    <div className="team-content">
+      <p className="team-name">{name}</p>
+      <p className="team-position">{position}</p>
+      <p className="team-location">{location}</p>
     </div>
+  </div>
 );
 
 TeamItem.propTypes = {
-    name: PropTypes.string,
-    location: PropTypes.string,
-    src: PropTypes.string,
-    alt: PropTypes.string,
-    position: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
 };
 
 export default TeamItem;
--- src/containers/Team/index.js
+++ src/containers/Team/index.js
@@ -1,34 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {
-    Footer,
-    Header,
-} from '../../components'
+  Footer,
+  Header,
+} from '../../components';
 
 import TeamItem from '../../components/TeamItem/index';
 import { teamItems } from '../../constants/index';
 
 const Team = ({ items }) => (
-    <div>
-        <div className="page-background">
-            <Header/>
-            <div className="team-header">
-                Team Carbon Doomsday
-            </div>
-            <div>
-                <div className="team-grid">
-                    {items.map((item) => (
-                        <TeamItem {...item} /> )
-                    )}
-                </div>
-            </div>
+  <div>
+    <div className="page-background">
+      <Header />
+      <div className="team-header">
+          Team Carbon Doomsday
+      </div>
+      <div>
+        <div className="team-grid">
+          {items.map((item) => {
+            return (
+              <TeamItem {...item} />
+            );
+          })}
         </div>
-        <Footer/>
+      </div>
     </div>
+    <Footer />
+  </div>
 );
 
+Team.propTypes = {
+  items: PropTypes.Array,
+};
+
 Team.defaultProps = {
-    items: teamItems
+  items: teamItems,
 };
 
 
