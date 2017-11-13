6efd9e0d433fba82f63e97706718c20401d8fc4f
grady-lad <ogradymartin7@gmail.com>
2017-11-13 17:30:00 -0500

    Changing to universal-ga.
    - react ga not supported with react 16

--- src/analytics.js
+++ src/analytics.js
@@ -1,11 +1,7 @@
-import ga from 'react-google-analytics';
-
-// TODO: Load these in via webpack or inject into html with webpack during the build
 export default function () {
   if (process.env.NODE_ENV === 'production') {
-    // google-analytics
-    ga('create', 'UA-02768398-1', 'auto');
-    ga('send', 'pageview');
+    const analytics = require('universal-ga'); //eslint-disable-line
+    analytics.initialize('UA-02768398-1', 'send');
     // sign up stuff
     document.write(
       '<script src="//load.sumome.com/" data-sumo-site-id="218d447ffc47e0e1d9bcf2ec3c2fe1a6f219761eed4cd1980142b9f9d7a5cd5c" async="async"></script>',
